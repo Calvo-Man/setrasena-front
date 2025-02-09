b8979040098dbc6d149597fb85b11cfe2021eb76
Miguel Angel Acuna <macunahernandez597@gmail.com>
2025-02-08 19:59:20 -0500

    vuetify agregado

--- src/main.js
+++ src/main.js
@@ -1,10 +1,21 @@
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
 
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
+import { createVuetify } from "vuetify";
+import "vuetify/styles";
+import * as components from "vuetify/components";
+import * as directives from "vuetify/directives";
+// import "@mdi/font/css/materialdesignicons.css";
 
-const app = createApp(App)
+const vuetify = createVuetify({
+  components,
+  directives,
+  ssr: true,
+});
 
-app.use(router)
+const app = createApp(App);
 
-app.mount('#app')
+app.use(router).use(vuetify);
+
+app.mount("#app");
